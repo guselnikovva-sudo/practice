f1835f988820e74f1748d677fa9b71671c11d4f3
guselnikovva <guselnikovva@mer.ci.nsu.ru>
2026-05-27 14:11:01 +0700

    MainActivity.kt

--- main/src/main/java/ci/nsu/moble/main/ui/ScreenOne.kt
+++ main/src/main/java/ci/nsu/moble/main/ui/ScreenOne.kt
@@ -3,16 +3,26 @@ package ci.nsu.moble.main.ui
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
 @Composable
 fun ScreenOne(
     modifier: Modifier = Modifier,
     onBackClick: () -> Unit
 ) {
+    var deposit by remember { mutableStateOf("") }
+    var month by remember { mutableStateOf("") }
+    var depositError by remember { mutableStateOf(false) }
+    var monthError by remember { mutableStateOf(false) }
+
     Box(modifier = Modifier.fillMaxSize()) {
         Text(
             text = "Основные параметры",
@@ -30,6 +40,34 @@ fun ScreenOne(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        OutlinedTextField(
+            value = deposit,
+            onValueChange = {
+                deposit = it
+                depositError = false
+            },
+            label = { Text("Стартовый взнос")},
+            isError = depositError,
+            supportingText = {
+                if (depositError) Text("Обязательное поле")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = month,
+            onValueChange = {
+                month = it
+                monthError = false
+            },
+            label = { Text("Срок вклада")},
+            isError = monthError,
+            supportingText = {
+                if (monthError) Text("Обязательное поле")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = onBackClick) {
             Text("Назад")
